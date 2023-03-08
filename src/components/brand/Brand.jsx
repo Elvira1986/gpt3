b30e0e882185d01364757e68a04c2c947688725a
Elvira Gizatullina <elvirawed@gmail.com>
2023-03-08 12:30:25 -0800

    Adding brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./brand.css";
+import { dropbox, google, shopify, slack, atlassian } from "./imports";
 
 export default function Brand() {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import dropbox from "../../assets/dropbox.png";
+import google from "../../assets/google.png";
+import shopify from "../../assets/shopify.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+
+export { dropbox, google, shopify, slack, atlassian };
